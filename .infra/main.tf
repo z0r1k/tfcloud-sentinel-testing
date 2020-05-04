5efea4943a46330d7f2db1bfac1ce85930f3a0a4
Mykhailo Lieibenson <mykhailo.lieibenson@audibene.de>
2020-05-04 16:53:25 +0200

    fixint errors

--- .infra/main.tf
+++ .infra/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  version = "2.33.0"
+  version = "~> 2.0"
   region  = var.aws_region
 }
 
@@ -27,10 +27,10 @@ resource "aws_lambda_function" "demo_lambda" {
   function_name = "tfcloud_gitops_demo_lambda"
 
   handler = "index.handler"
-  runtime = "nodejs12.x"
+  runtime = "nodejs10.x"
 
   filename         = "function.zip"
-  source_code_hash = base64sha256(file("function.zip"))
+  source_code_hash = filebase64sha256("../function.zip")
 
   role = aws_iam_role.iam_for_lambda.arn
 
