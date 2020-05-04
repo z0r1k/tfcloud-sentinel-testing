10fe4c57887ca66a2ab5b5fe6bc76d37e7661279
Mykhailo Lieibenson <mykhailo.lieibenson@audibene.de>
2020-05-04 21:09:18 +0200

    adding ec2 instance + policy

--- .infra-policies/sentinel.hcl
+++ .infra-policies/sentinel.hcl
@@ -1,3 +1,7 @@
 policy "enforce-mandatory-tags" {
+    enforcement_level = "hard-mandatory"
+}
+
+policy "validate-instance-type" {
     enforcement_level = "soft-mandatory"
 }
--- .infra/main.tf
+++ .infra/main.tf
@@ -46,3 +46,30 @@ resource "aws_lambda_function" "tfc_gitops_demo_lambda" {
     billable   = var.tag_billable
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    user_name  = var.tag_user_name
+    department = var.tag_department
+    billable   = var.tag_billable
+  }
+}
